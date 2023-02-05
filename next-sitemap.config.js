0add887e252bfc3e2428ca90784eafef3c0eb0d7
Naman Arora <namanarora1022@gmail.com>
2023-02-05 21:13:14 +0530

    added sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL,
+    siteUrl: 'https://namanarora.vercel.app',
     generateRobotsTxt: true,
     robotsTxtOptions: {
         policies: [{ userAgent: '*', allow: '/' }]
