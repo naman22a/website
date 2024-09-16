9827034797faa925b5dcc28e1823084682b8fd94
Naman Arora <namanarora1022@gmail.com>
2024-09-16 20:30:02 +0530

    feat: 🎨 skillset

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,4 +12,22 @@ const landing = defineCollection({
     })
 });
 
-export const collections = { landing };
+const skillset = defineCollection({
+    type: 'data',
+    schema: z.object({
+        smallText: z.string(),
+        largeText1: z.string(),
+        largeText2: z.string(),
+        buttonText: z.string(),
+        description: z.string(),
+        skills: z.array(
+            z.object({
+                name: z.string(),
+                icon: z.string(),
+                href: z.string()
+            })
+        )
+    })
+});
+
+export const collections = { landing, skillset };
