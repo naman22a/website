b4597cbed0ade294606eaae9c9aa090301fae454
Naman Arora <namanarora1022@gmail.com>
2024-10-14 19:13:40 +0530

    feat: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
     server: { port: 3000 },
@@ -19,5 +20,8 @@ export default defineConfig({
     },
     redirects: {
         '/blogs': '/blogs/1'
-    }
+    },
+    adapter: vercel({
+        webAnalytics: { enabled: true }
+    })
 });
