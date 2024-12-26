c496221bec130d92221e1ac9ccd60c7e64d2941c
Naman Arora <namanarora1022@gmail.com>
2024-12-26 09:38:07 +0530

    feat: custom form

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -1,17 +1,13 @@
-import { type Component } from 'solid-js';
+import { createSignal, type Component } from 'solid-js';
 import { IoLocationSharp, IoMail } from 'solid-icons/io';
 import { BsGithub, BsLinkedin } from 'solid-icons/bs';
 import { FaBrandsInstagram, FaBrandsTwitter } from 'solid-icons/fa';
-import { Form, type FormType } from 'solid-js-form';
-import type { Store } from 'solid-js/store';
-import Input from './Input';
-import TextArea from './TextArea';
-import type { ContactFormValues } from '../../interfaces';
 import { RiBusinessSendPlaneFill } from 'solid-icons/ri';
 import Button from '../shared/Button';
 import emailjs from '@emailjs/browser';
 import toast, { Toaster } from 'solid-toast';
 import { isEmail } from '../../utils';
+import { twMerge } from 'tailwind-merge';
 
 interface Props {
     smallText: string;
@@ -31,12 +27,19 @@ const Contact: Component<Props> = ({
     largeText,
     description1,
     description2,
-    email,
+    email: myEmail,
     githubLink,
     instagramLink,
     twitterLink,
     linkedinLink
 }) => {
+    const [name, setName] = createSignal('');
+    const [nameError, setNameError] = createSignal('');
+    const [email, setEmail] = createSignal('');
+    const [emailError, setEmailError] = createSignal('');
+    const [message, setMessage] = createSignal('');
+    const [messageError, setMessageError] = createSignal('');
+
     return (
         <div
             class="my-20 flex flex-col justify-center items-center"
@@ -49,102 +52,65 @@ const Contact: Component<Props> = ({
 
             <div class="flex flex-col md:flex-row">
                 <div class="md:w-1/2 grid place-items-center">
-                    <Form
+                    <form
                         class="mt-5 min-w-full md:px-10"
-                        initialValues={
-                            {
-                                name: '',
-                                email: '',
-                                message: ''
-                            } satisfies ContactFormValues
-                        }
-                        onSubmit={
-                            (async (
-                                form: Store<FormType.Context<ContactFormValues>>
-                            ) => {
-                                const { name, email, message } = form.values;
+                        onSubmit={async (e) => {
+                            e.preventDefault();
+                            setNameError('');
+                            setEmailError('');
+                            setMessageError('');
 
-                                try {
-                                    // validation
-                                    if (!name) {
-                                        form.setError(
-                                            'name',
-                                            'Name is required.'
-                                        );
-                                        return;
-                                    }
+                            try {
+                                // validation
+                                if (!name()) {
+                                    setNameError('Name is required.');
+                                    return;
+                                }
 
-                                    if (!email) {
-                                        form.setError(
-                                            'email',
-                                            'Email is required.'
-                                        );
-                                        return;
-                                    }
+                                if (!email()) {
+                                    setEmailError('Email is required.');
+                                    return;
+                                }
 
-                                    if (!message) {
-                                        form.setError(
-                                            'message',
-                                            'Message is required.'
-                                        );
-                                        return;
-                                    }
+                                if (!message()) {
+                                    setMessageError('Message is required.');
+                                    return;
+                                }
 
-                                    if (!isEmail(email)) {
-                                        form.setError(
-                                            'email',
-                                            'Invalid email.'
-                                        );
-                                        return;
-                                    }
+                                if (!isEmail(email())) {
+                                    setEmailError('Invalid email.');
+                                    return;
+                                }
 
-                                    const res = await emailjs.send(
-                                        import.meta.env
-                                            .PUBLIC_EMAILJS_SERVICE_ID,
-                                        import.meta.env
-                                            .PUBLIC_EMAILJS_TEMPLATE_ID,
-                                        {
-                                            name,
-                                            email,
-                                            message
+                                const res = await emailjs.send(
+                                    import.meta.env.PUBLIC_EMAILJS_SERVICE_ID,
+                                    import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID,
+                                    {
+                                        name: name(),
+                                        email: email(),
+                                        message: message()
+                                    },
+                                    {
+                                        limitRate: {
+                                            throttle: 10000 // 10 secs
                                         },
+                                        publicKey: import.meta.env
+                                            .PUBLIC_EMAILJS_PUBLIC_KEY
+                                    }
+                                );
+                                if (res.status === 200) {
+                                    toast.success(
+                                        'Thank you. I will get back to you as soon as possible.',
                                         {
-                                            limitRate: {
-                                                throttle: 10000 // 10 secs
-                                            },
-                                            publicKey: import.meta.env
-                                                .PUBLIC_EMAILJS_PUBLIC_KEY
+                                            position: 'top-center',
+                                            style: {
+                                                'background-color': '#1e1e2e',
+                                                color: '#cdd6f4',
+                                                'text-align': 'center'
+                                            }
                                         }
                                     );
-                                    if (res.status === 200) {
-                                        toast.success(
-                                            'Thank you. I will get back to you as soon as possible.',
-                                            {
-                                                position: 'top-center',
-                                                style: {
-                                                    'background-color':
-                                                        '#1e1e2e',
-                                                    color: '#cdd6f4',
-                                                    'text-align': 'center'
-                                                }
-                                            }
-                                        );
-                                    } else {
-                                        toast.error(
-                                            'Opps! Something went wrong.',
-                                            {
-                                                position: 'top-center',
-                                                style: {
-                                                    'background-color':
-                                                        '#1e1e2e',
-                                                    color: '#cdd6f4',
-                                                    'text-align': 'center'
-                                                }
-                                            }
-                                        );
-                                    }
-                                } catch (error) {
-                                    console.error(error);
+                                } else {
                                     toast.error('Opps! Something went wrong.', {
                                         position: 'top-center',
                                         style: {
@@ -154,38 +120,101 @@ const Contact: Component<Props> = ({
                                         }
                                     });
                                 }
-                            }) as any
-                        }
+                            } catch (error) {
+                                console.error(error);
+                                toast.error('Opps! Something went wrong.', {
+                                    position: 'top-center',
+                                    style: {
+                                        'background-color': '#1e1e2e',
+                                        color: '#cdd6f4',
+                                        'text-align': 'center'
+                                    }
+                                });
+                            }
+                        }}
                     >
-                        <Input
-                            name="name"
-                            label="Name"
-                            placeholder="What's your good name ?"
-                        />
-                        <Input
-                            name="email"
-                            label="Email"
-                            placeholder="What's your web address ?"
-                            type="email"
-                        />
-                        <TextArea
-                            rows={7}
-                            name="message"
-                            label="Message"
-                            placeholder="What's you want to say ?"
-                        />
+                        {/* Name */}
+                        <div class="flex flex-col my-5">
+                            <label for="name" class="mb-2 text-lg">
+                                Name
+                            </label>
+                            <input
+                                class={twMerge(
+                                    'px-5 py-2 bg-ctp-crust rounded outline-none border-b-2 border-b-transparent focus-within:border-b-ctp-red',
+                                    nameError() && 'outline-1 outline-red-600'
+                                )}
+                                name="name"
+                                placeholder="What's your good name ?"
+                                type="text"
+                                value={name()}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <span class="text-sm font-semibold text-red-600 mt-2">
+                                {' '}
+                                {nameError()}
+                            </span>
+                        </div>
+
+                        {/* Email */}
+
+                        <div class="flex flex-col my-5">
+                            <label for="email" class="mb-2 text-lg">
+                                Email
+                            </label>
+                            <input
+                                class={twMerge(
+                                    'px-5 py-2 bg-ctp-crust rounded outline-none border-b-2 border-b-transparent focus-within:border-b-ctp-red',
+                                    emailError() && 'outline-1 outline-red-600'
+                                )}
+                                name="email"
+                                placeholder="What's your web address ?"
+                                type="email"
+                                value={email()}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <span class="text-sm font-semibold text-red-600 mt-2">
+                                {' '}
+                                {emailError()}
+                            </span>
+                        </div>
+
+                        {/* Message */}
+                        <div class="flex flex-col my-5">
+                            <label for="message" class="mb-2 text-lg">
+                                Message
+                            </label>
+                            <textarea
+                                class={twMerge(
+                                    'px-5 py-2 bg-ctp-crust rounded outline-none border-b-2 border-b-transparent focus-within:border-b-ctp-red',
+                                    messageError() &&
+                                        'outline-1 outline-red-600'
+                                )}
+                                name="message"
+                                placeholder="What's "
+                                value={message()}
+                                onChange={(e) => setMessage(e.target.value)}
+                                rows={7}
+                            />
+                            <span class="text-sm font-semibold text-red-600 mt-2">
+                                {' '}
+                                {messageError()}
+                            </span>
+                        </div>
+
                         <Button type="submit">
                             <div class="flex items-center">
                                 <RiBusinessSendPlaneFill class="mr-3 h-6 w-6" />
                                 <span>Send Email</span>
                             </div>
                         </Button>
-                    </Form>
+                    </form>
                 </div>
                 <div class="md:w-1/2 flex flex-col pt-12">
                     <p class="">
                         {description1}{' '}
-                        <span class="font-bold text-ctp-sapphire">{email}</span>{' '}
+                        <span class="font-bold text-ctp-sapphire">
+                            {myEmail}
+                        </span>{' '}
                         {description2}
                     </p>
                     <div class="font-JetBrains mt-7">
@@ -201,7 +230,7 @@ const Contact: Component<Props> = ({
 
                         <div class="flex my-2">
                             <IoMail class="h-5 w-5 mr-3 text-ctp-green" />
-                            <a href={`mailto:${email}`}>{email}</a>
+                            <a href={`mailto:${myEmail}`}>{myEmail}</a>
                         </div>
                     </div>
                     <div class="flex mt-5">
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,9 +2,3 @@ export interface PageRoute {
     name: string;
     href: string;
 }
-
-export interface ContactFormValues {
-    name: string;
-    email: string;
-    message: string;
-}
